14dd4963d2e0e2b21cbe29aed9dd2def441832e7
Mark Somerville <mark@scottishclimbs.com>
2009-12-01 14:20:19 +0000

    SupportChecker builds a GStreamer pipeline and detects if a file is playable.
    This is a very slow but robust way of checking for Mozart capability.
    
    Requires ruby-gstreamer.

---
+++ test/support_checker_test.rb
@@ -0,0 +1,17 @@
+require 'test/unit'
+require 'fileutils'
+require_relative "../lib/support_checker"
+
+class SupportCheckerTest < Test::Unit::TestCase
+  def test_support_ogg
+    assert MusicImporter::SupportChecker.new.supported? "test/music/dlanod.ogg"
+  end
+
+  def test_support_mp3
+    assert MusicImporter::SupportChecker.new.supported? "test/music/dlanod.mp3"
+  end
+
+  def test_do_no_support_text
+    assert !MusicImporter::SupportChecker.new.supported?(__FILE__)
+  end
+end
