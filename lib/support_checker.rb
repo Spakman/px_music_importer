7aab131a38e23ae351bf1296c41ab0a9bd50d459
Mark Somerville <mark@scottishclimbs.com>
2009-12-02 09:26:37 +0000

    Switched the SupportChecker to using a playbin.
    It would be better to use a playbin2 (like Mozart), but that causes
    hangs sometimes when trying to stop the playbin.

--- lib/support_checker.rb
+++ lib/support_checker.rb
@@ -2,12 +2,22 @@ require 'gst'
 
 module MusicImporter
   class MusicImporter::SupportChecker
-    def initialize
+    # Returns if a file is supported by our audio system.
+    #
+    # This will return true for video files that have an audio track.
+    #
+    # This feels rather brute force and there is likely to be a nicer (shorter,
+    # faster) way to check, but I don't know what it is.
+    def supported?(filepath)
+      @supported = false
+      # it would be better if this was using playbin2, but that causes hangs sometimes when stopping the playbin.
+      @playbin = Gst::ElementFactory.make('playbin')
+      @playbin.uri = "file:///#{File.expand_path(filepath)}"
+      @playbin.ready
       @loop = GLib::MainLoop.new(nil, false)
-      @pipeline = Gst::Pipeline.new
+      @playbin.audio_sink = Gst::ElementFactory.make('autoaudiosink')
 
-      # The sole job is to catch messages, set the @supported state and immediately quit the loop.
-      @pipeline.bus.add_watch do |bus, message|
+      @playbin.bus.add_watch do |bus, message| 
         case message.type
         when Gst::Message::TAG
           @supported = true
@@ -18,30 +28,12 @@ module MusicImporter
         end
         true
       end
-    end
 
-    # Returns if a file is supported by our audio system.
-    #
-    # This will return true for video files that have an audio track.
-    #
-    # This feels rather brute force and there is likely to be a nicer (shorter,
-    # faster) way to check, but I don't know what it is.
-    def supported?(filepath)
-      @supported = false
-      decoder = Gst::ElementFactory.make("decodebin")
-      audio_convert = Gst::ElementFactory.make("audioconvert")
-      audio_resample = Gst::ElementFactory.make("audioresample")
-      audio_sink = Gst::ElementFactory.make("autoaudiosink")
-      file_src = Gst::ElementFactory.make("filesrc")
-      file_src.location = filepath
-      @pipeline.add(file_src, decoder, audio_convert, audio_resample, audio_sink)
-      file_src >> decoder
-      audio_convert >> audio_resample >> audio_sink
       begin
-        @pipeline.play
+        @playbin.play
         @loop.run
       ensure
-        @pipeline.stop
+        @playbin.stop
       end
       return @supported
     end
